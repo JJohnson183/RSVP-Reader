136b0c9f55ee1b198645726dfe458d3748f35095
Jordan Johnson <Johnson184640@gmail.com>
2026-02-28 21:02:53 -0500

    Hid title in read mode

--- app/page.tsx
+++ app/page.tsx
@@ -21,18 +21,23 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
       <main className="w-full max-w-4xl p-8">
 
-         {/*============ Title and subtitle =============*/}
-        <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 dark:text-white">
-          RSVP Fast Reader
-        </h1>
-        
-        <p className="text-center text-gray-600 dark:text-gray-300 mb-8">
-          This is a reader that implements the Rapid Serial Visual Presentation (RSVP) technique to help you read faster.
-        </p>
-
         {/*=== Determine which view to show: Text input or reading ===*/}
         { !isReading 
-            ? (<TextInputView text={text} setText={setText} setIsReading={setIsReading}/>) 
+            ? (
+              <div>
+                {/*============ Title and subtitle =============*/}
+                <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 dark:text-white">
+                  RSVP Fast Reader
+                </h1>
+                
+                <p className="text-center text-gray-600 dark:text-gray-300 mb-8">
+                  This is a reader that implements the Rapid Serial Visual Presentation (RSVP) technique to help you read faster.
+                </p>
+
+                {/*============ Text input and start button =============*/}
+                <TextInputView text={text} setText={setText} setIsReading={setIsReading}/>
+              </div>
+            ) 
             : (<ReadingView setIsReading={setIsReading}/>)
         }
         
